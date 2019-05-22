c60b27a6a92a5c05971892a3938135842ecdd5f5
Evgeniy <jerimof@gmail.com>
2019-05-22 17:19:53 +0300

    Removed outdated code

--- styleguide/routes.js
+++ styleguide/routes.js
@@ -3,28 +3,21 @@ import { sortBy, filter } from 'lodash';
 
 const context = require.context('./pages', true, /\.(js|mdx)$/);
 
-const routes = context
-  .keys()
-  .map(key => {
-    const { dir, ext, name } = pathParse(key.replace(/\.\//, ''));
-    const path = key
-      .replace(ext, '')
-      .replace(/\./, '')
-      .replace(/\/index/, '');
+const routes = context.keys().map(key => {
+  const { dir, ext, name } = pathParse(key.replace(/\.\//, ''));
+  const path = key
+    .replace(ext, '')
+    .replace(/\./, '')
+    .replace(/\/index/, '');
 
-    // exclude file names with _
-    if (name.match(/^_/) !== null) return null;
-    if (dir.match(/^_/) !== null) return null;
-
-    return {
-      key,
-      dirname: dir,
-      name: name === 'index' && dir ? dir : name,
-      path: path || '/',
-      component: context(key).default
-    };
-  })
-  .filter(el => el !== null);
+  return {
+    key,
+    dirname: dir,
+    name: name === 'index' && dir ? dir : name,
+    path: path || '/',
+    component: context(key).default
+  };
+});
 
 const organisedRoutes = routes.reduce((accum, route, i, array) => {
   const { dirname, key } = route;
